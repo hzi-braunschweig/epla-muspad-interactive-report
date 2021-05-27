064d0b01b8396cd2273f9f645ec2805a172ff783
Stéphane Ghozzi <1541878-stephaneghozzi@users.noreply.gitlab.com>
2021-05-27 11:47:21 +0200

    updates data paths

--- R/data-paths.R
+++ R/data-paths.R
@@ -2,10 +2,10 @@
 
 #Local assuming data files were copied in the project directory
 labresults_path <- "data"
-logs_path <- "data/TempLogs"
-bloodsamples_path <- "data/Blutprobenlisten"
+logs_path <- "data/05_TempLogs"
+bloodsamples_path <- "data/01_Blutprobenlisten"
 
 #At EPID/HZI, assuming the project directory lies in EPLA/MUSPAD-1/
 # labresults_path <- "../../../PROJACTIVE/MUSPAD-LABORERGEBNISSE"
-# logs_path <- "../04_TempLogs"
+# logs_path <- "../05_TempLogs"
 # bloodsamples_path <- "../01_Blutprobenlisten"
